694a3e46c0b648eba0e5545d59206665704bfb98
Victor Yoshiaki Miyai <vymiyai@hotmail.com>
2014-02-10 23:57:58 +0000

    Adicionando arquivo de configuracao.

--- src/script.js
+++ src/script.js
@@ -1,87 +1,51 @@
 window.addEventListener('load',function(e) 
 {
-	// Set up a standard Quintus instance with only the 
-  	// Sprites and Scene module (for the stage support) loaded.
+ 	// Set up Quintus instance____________________________________________________
   	var Q = window.Q = Quintus().
-    	include("Sprites, Scenes, 2D, Input").
-    	setup({ width: 480, height: 800 });
+    	include("Sprites, Scenes, Input").
+    	setup({ width: config.screen.width, height: config.screen.height });
   
   
   
-  
-  
-  
-  	Q.ASSET = { "ENEMY":"enemy.png", "SUWAKO":"suwako.png" };
-
-        Q.Sprite.extend( "CHARACTER", 
+ 	// Character class definition_________________________________________________
+	Q.Sprite.extend( "Character", 
+	{
+		init: function( sheetName ) 
 		{
-       		init: function( assetName ) 
-          	{
-         		this._super(
-                {
-                  	sheet: "character",
-                  	frame: ZAH,
-                  	h: 514,
-                  	w: 514
-         		});
-       		}
-     	});
+			this._super(
+			{
+				sheet: sheetName,
+				frame: config.sprites.actions.idle,
+				h: config.sprites.portraits.height,
+				w: config.sprites.portraits.width
+			});
+		}
+	});
         
-        
-    /*
-  	Q.CHARACTER = function( assetName )
-  	{
-  		//return new Q.Sprite({ x: 500, y: 100, asset: assetName, angle: 0, collisionMask: 1, scale: 1}); 
-    
-   	 	//return new Q.Sprite({ asset: Q.ASSET[ assetName ] }); 
-      
-      	return new Q.Sprite({ sheet: "GermaniaSpritesheets" }); 
-      
-        Q.Sprite.extend("Penguin", 
-		{
-       		init: function(p) 
-          	{
-         		this._super(
-                {
-           			sheet: "player",
-           			frame: 7
-         		});
-       		}
-     	});
-  	};
-*/
 
 
 
-	
-
-  
-  
-  
-  
-  
   
   	// Create a simple scene that adds two shapes on the page
   	Q.scene("start",function(stage) 
     {
-      	/*
-      	// A basic sprite shape a asset as the image
-      	var sprite1 = new Q.Sprite({ x: 500, y: 100, asset: 'enemy.png', 
-                                   angle: 0, collisionMask: 1, scale: 1});
-      	*/
-    
-        var sprite1 = new Q.CHARACTER( 'SUWAKO' );
-        sprite1.set( { x: 240, y: 257 } );
+        var sprite1 = new Q.Character( 'character' );
+      
+        sprite1.set( { x: config.screen.width/2, y: config.sprites.portraits.height/2 } );
         
         stage.insert(sprite1);
         
         sprite1.on('step',function() { });
 
         // A red platform for the other sprite to land on
-        var sprite2 = new Q.Sprite({ x: 240, y: 600, w: 300, h: 200 });
+        var sprite2 = new Q.Sprite({ 	x: config.dialogBox.x, 
+                                    	y: config.dialogBox.y, 
+                                    	w: config.dialogBox.width, 
+                                    	h: config.dialogBox.height
+                                   		});
         sprite2.draw= function(ctx) 
         {
-            ctx.fillStyle = '#FF0000';
+            ctx.fillStyle = '#0000FF';
             ctx.fillRect(-this.p.cx,-this.p.cy,this.p.w,this.p.h);
     	};
       
