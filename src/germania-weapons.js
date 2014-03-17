9ab5bc2341a6b12e192685c3e402e6857c0008a0
Victor Yoshiaki Miyai <vymiyai@hotmail.com>
2014-03-17 12:56:13 +0000

    Adicionando precisao variavel em Rifle.

--- src/germania-weapons.js
+++ src/germania-weapons.js
@@ -2,4 +2,4 @@
 
 var WEAPONS = {};
 
-WEAPONS[ "RIFLE" ] = new Weapon( "Rifle", 5, 10, function( distance ){ return 1; }, function( distance ){ return 1; } );
+WEAPONS[ "RIFLE" ] = new Weapon( "Rifle", 5, 10, function( distance ){ return 1; }, function( distance ){ var distanceMap = [ 0.5, 0.9, 0.8, 0.7, 0.6 ]; return distanceMap[ distance ]; } );
