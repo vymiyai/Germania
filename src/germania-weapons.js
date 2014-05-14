365adc887c451645d06f3670d730d2f4c91802e5
Victor Yoshiaki Miyai <vymiyai@hotmail.com>
2014-05-14 16:14:34 +0000

    Reestruturando o esquema de abilities, adicionando somente bonus ao se equipar armas.

--- src/germania-weapons.js
+++ src/germania-weapons.js
@@ -1,6 +1,56 @@
 "use strict";
 
+// Luger P08 mais precisa que a P38, mas mais rara.
+
 var WEAPONS = {};
 
-WEAPONS.RIFLE = { name:"Rifle", minimumDamage:5, maximumDamage:10, distanceDamageModifier:function( distance ){ var distanceMap = [ 1.0, 1.0, 0.9, 0.8, 0.7 ]; return distanceMap[ distance ]; }, distanceAccuracyModifier:function( distance ){ var distanceMap = [ 0.5, 0.9, 0.8, 0.7, 0.6 ]; return distanceMap[ distance ]; }, ability:function( target ){ target.primary.getBaseDamage = ABILITIES.WEAPON_ABILITIES.MIGHTY_SHOT( target ); } };
-WEAPONS.SCOPED_RIFLE = { name:"Scoped rifle", minimumDamage:5, maximumDamage:10, distanceDamageModifier:function( distance ){ var distanceMap = [ 1.0, 1.0, 0.9, 0.8, 0.7 ]; return distanceMap[ distance ]; }, distanceAccuracyModifier:function( distance ){ var distanceMap = [ 0.5, 0.9, 0.8, 0.7, 0.6 ]; return distanceMap[ distance ]; }, ability:function( target ){ target.primary.getBaseDamage = ABILITIES.WEAPON_ABILITIES.MIGHTIER_SHOT( target ); target.primary.getBaseAccuracy = ABILITIES.WEAPON_ABILITIES.SURE_SHOT; /*target.addAbility( function(){ alert( "ABILITY ACTIVATED - NO MISS!" ); } );*/ } };
+// usando as medidas no extremo das tabelas no Wikipedia bullet weight x bullet diameter, o dano seria..
+// Mauser 7.92x57mm 12,8 * 8,08 = 103,424 +/- 20%
+// Parabellum 9x19mm 7,45 * 9,01 = 67,1245 +/- 20%
+
+// Sniper sovietico pra fins de estatistica: 50% 800m, 80% 500m
+
+WEAPONS.KARABINER_98K           = { name:"Karabiner 98k", 
+                                    category: "PRIMARY", 
+                                    type:"RIFLE",
+                                    minimumDamage:5, 
+                                    maximumDamage:10, 
+                                    distanceDamageModifier:function( distance ){ var distanceMap = [ 1.0, 1.0, 0.9, 0.8, 0.7 ]; return distanceMap[ distance ]; }, 
+                                    distanceAccuracyModifier:function( distance ){ var distanceMap = [ 0.5, 0.9, 0.8, 0.7, 0.6 ]; return distanceMap[ distance ]; }, 
+                                    bonuses:{} };
+                                    
+WEAPONS.SCOPED_KARABINER_98K    = { name:"Scoped Karabiner 98k", 
+                                    category: "PRIMARY", 
+                                    type:"RIFLE",
+                                    minimumDamage:5, 
+                                    maximumDamage:10, 
+                                    distanceDamageModifier:function( distance ){ var distanceMap = [ 1.0, 1.0, 0.9, 0.8, 0.7 ]; return distanceMap[ distance ]; }, 
+                                    distanceAccuracyModifier:function( distance ){ var distanceMap = [ 0.5, 0.9, 0.8, 0.7, 0.6 ]; return distanceMap[ distance ]; }, 
+                                    bonuses:{} };
+                                    
+WEAPONS.LUGER_P08               = { name:"Luger P08", 
+                                    category: "SECONDARY", 
+                                    type:"PISTOL",
+                                    minimumDamage:5, 
+                                    maximumDamage:10, 
+                                    distanceDamageModifier:function( distance ){ var distanceMap = [ 1.0, 1.0, 0.9, 0.8, 0.7 ]; return distanceMap[ distance ]; }, 
+                                    distanceAccuracyModifier:function( distance ){ var distanceMap = [ 0.5, 0.9, 0.8, 0.7, 0.6 ]; return distanceMap[ distance ]; }, 
+                                    bonuses:{} };
+                                    
+WEAPONS.WALTHER_P38             = { name:"Walther P38", 
+                                    category: "SECONDARY", 
+                                    type:"PISTOL",
+                                    minimumDamage:5, 
+                                    maximumDamage:10, 
+                                    distanceDamageModifier:function( distance ){ var distanceMap = [ 1.0, 1.0, 0.9, 0.8, 0.7 ]; return distanceMap[ distance ]; }, 
+                                    distanceAccuracyModifier:function( distance ){ var distanceMap = [ 0.5, 0.9, 0.8, 0.7, 0.6 ]; return distanceMap[ distance ]; }, 
+                                    bonuses:{} };
+
+WEAPONS.MG_34                   = { name:"Maschinengewehr 34", 
+                                    category: "PRIMARY", 
+                                    type:"MACHINE_GUN",
+                                    minimumDamage:5, 
+                                    maximumDamage:10, 
+                                    distanceDamageModifier:function( distance ){ var distanceMap = [ 1.0, 1.0, 0.9, 0.8, 0.7 ]; return distanceMap[ distance ]; }, 
+                                    distanceAccuracyModifier:function( distance ){ var distanceMap = [ 0.5, 0.9, 0.8, 0.7, 0.6 ]; return distanceMap[ distance ]; }, 
+                                    bonuses:{ BASIC_SPEED: -2 } };
